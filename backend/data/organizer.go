525bfbf2c08eb079508b6832215edd7af6297de6
aria-yousefi <aria.yousefi@gmail.com>
2025-04-18 17:56:08 -0400

    ScrapeGainesvilleSun() now appropriately scrapes organizer info, creating organizer

--- backend/data/organizer.go
+++ backend/data/organizer.go
@@ -2,11 +2,11 @@ package data
 
 // Organizer represents the organizer model
 type Organizer struct {
-	ID             uint     `json:"id" gorm:"primaryKey"`
-	Name           string   `json:"name"`
-	Email          string   `json:"email" gorm:"unique"`
-	Password       string   `json:"password"`
-	Description    string   `json:"description"`    // Organizer description
-	ContactDetails  string   `json:"contact_details"` // Contact details for the organizer
-	Events         []Event  `gorm:"foreignKey:OrganizerID"` // One-to-many relationship
+	ID             uint    `json:"id" gorm:"primaryKey"`
+	Name           string  `json:"name"`
+	Email          string  `json:"email" gorm:"unique,default:null"` // Allow null to ignore unique constraint when empty
+	Password       string  `json:"password"`
+	Description    string  `json:"description"`            // Organizer description
+	ContactDetails string  `json:"contact_details"`        // Contact details for the organizer
+	Events         []Event `gorm:"foreignKey:OrganizerID"` // One-to-many relationship
 }
